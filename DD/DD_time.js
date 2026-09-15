62416d2f9af300b51ff37bcab8127fab0957f111
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-15 22:02:02 +0900

    Reset DD hunger state on new game

--- DD/DD_time.js
+++ DD/DD_time.js
@@ -126,6 +126,7 @@
       sessionStorage.removeItem('dd_session_inventory_v1');
       sessionStorage.removeItem('dd_session_events_v1');
       sessionStorage.removeItem('dd_session_wolf_v1');
+      sessionStorage.removeItem('dd_session_needs_v1');
       sessionStorage.removeItem(SESSION_KEY);
     } catch (_) {}
     clearLegacyPersistence();
