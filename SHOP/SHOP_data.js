04f98ae25f224d1abc25b033aa56b8e9605bd6d6
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-22 22:06:50 +0900

    Add provisional SHOP scene backgrounds

--- SHOP/SHOP_data.js
+++ SHOP/SHOP_data.js
@@ -12,10 +12,47 @@
       temperature: 24,
       weather: '晴れ',
       scene: '小門外・底辺区',
+      sceneKey: 'outerPoor',
       hp: 100,
       hunger: 25,
       thirst: 30,
       money: 0
+    }),
+
+    scenes: Object.freeze({
+      outerPoor: Object.freeze({
+        name: '小門外・底辺区',
+        width: 1600,
+        background: './asset/bg_desert_wall.jpeg',
+        objects: Object.freeze([
+          Object.freeze({src:'./asset/house_poor.png', left:35, height:78, layer:2}),
+          Object.freeze({src:'./asset/house_poor.png', left:410, height:72, layer:2, flip:true}),
+          Object.freeze({src:'./asset/stall.png', left:790, height:61, layer:4}),
+          Object.freeze({src:'./asset/house_poor.png', left:1040, height:75, layer:2}),
+          Object.freeze({src:'./asset/gate_small.png', left:1250, height:95, layer:5})
+        ])
+      }),
+      cityCommon: Object.freeze({
+        name: '市内・庶民街',
+        width: 1600,
+        background: './asset/bg_desert_wall.jpeg',
+        objects: Object.freeze([
+          Object.freeze({src:'./asset/house_normal.png', left:70, height:82, layer:2}),
+          Object.freeze({src:'./asset/stall.png', left:520, height:62, layer:4}),
+          Object.freeze({src:'./asset/house_normal.png', left:860, height:80, layer:2, flip:true}),
+          Object.freeze({src:'./asset/gate_small.png', left:1240, height:94, layer:5})
+        ])
+      }),
+      upperArea: Object.freeze({
+        name: '上級区',
+        width: 1600,
+        background: './asset/bg_desert_wall.jpeg',
+        objects: Object.freeze([
+          Object.freeze({src:'./asset/house_upper.png', left:90, height:88, layer:2}),
+          Object.freeze({src:'./asset/house_upper.png', left:640, height:84, layer:2, flip:true}),
+          Object.freeze({src:'./asset/house_normal.png', left:1130, height:76, layer:2})
+        ])
+      })
     }),
 
     labels: Object.freeze({
