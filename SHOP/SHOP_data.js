8594677c3f20ea5c4e4381096e5e608e7aaa8b16
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-23 08:22:35 +0900

    Force fresh SHOP top page from menu links

--- SHOP/SHOP_data.js
+++ SHOP/SHOP_data.js
@@ -95,8 +95,8 @@
     }),
 
     links: Object.freeze({
-      newGame: './SHOP_top.html?new=1',
-      continueGame: './SHOP_top.html?continue=1',
+      newGame: './SHOP_top.html?new=1&build=20260923-0842',
+      continueGame: './SHOP_top.html?continue=1&build=20260923-0842',
       backToGames: '../index.html',
       backToShopMenu: './SHOP_index.html'
     })
