d0c300d5d56d03787ce8f284c9c7f95609483100
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-22 17:45:12 +0900

    Add SHOP HUD and player state data

--- SHOP/SHOP_data.js
+++ SHOP/SHOP_data.js
@@ -4,26 +4,40 @@
   window.SHOP_DATA = Object.freeze({
     title: 'お店屋さん',
     subtitle: '砂漠都市で暮らし、商いを始める',
+
     initialState: Object.freeze({
-      version: 1,
+      version: 2,
       day: 1,
       minutes: 360,
+      temperature: 24,
+      weather: '晴れ',
+      scene: '小門外・底辺区',
+      hp: 100,
+      hunger: 25,
+      thirst: 30,
       money: 0
     }),
-    layout: Object.freeze({
-      shelfSlots: 9
-    }),
+
     labels: Object.freeze({
       newGame: '新しく始める',
       continueGame: '続きから',
       backToGames: 'ゲーム一覧へ戻る',
-      floor: '店内',
+      day: '日にち',
+      time: '時間',
+      temperature: '気温',
+      weather: '天気',
+      hp: 'HP',
+      hunger: '飢え',
+      thirst: '渇き',
+      portrait: '主人公',
+      townPlaceholder: '町の背景絵',
+      map: '地図',
+      inventory: '持ち物',
+      action: '行動',
       save: 'セーブ',
-      load: 'ロード',
-      back: '戻る',
-      money: '所持金',
-      daySuffix: '日目'
+      load: 'ロード'
     }),
+
     links: Object.freeze({
       newGame: './SHOP_top.html?new=1',
       continueGame: './SHOP_top.html?continue=1',
