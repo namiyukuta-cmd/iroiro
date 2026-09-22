70cf39b58a6ac0181a490aa5fc5f69e8ed2c947c
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-23 08:17:07 +0900

    Increase SHOP NPCs to readable mobile size

--- SHOP/SHOP_data.js
+++ SHOP/SHOP_data.js
@@ -36,8 +36,8 @@
           Object.freeze({src:'./asset/house_poor.png', left:26, bottom:5.2, height:28, layer:'town', flip:true}),
           Object.freeze({src:'./asset/house_poor.png', left:55, bottom:5.5, height:31, layer:'town'}),
           Object.freeze({src:'./asset/house_poor.png', left:80, bottom:5.2, height:27, layer:'town', flip:true}),
-          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_oldman_01.png', left:23, bottom:5.8, height:13.5, layer:'people'}),
-          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_porter_01.png', left:68, bottom:5.8, height:14, layer:'people'})
+          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_oldman_01.png', left:23, bottom:4.8, height:24, layer:'people'}),
+          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_porter_01.png', left:68, bottom:4.8, height:24, layer:'people'})
         ])
       }),
       smallGateOutside: Object.freeze({
@@ -47,8 +47,8 @@
           Object.freeze({src:'./asset/house_poor.png', left:-7, bottom:5.5, height:27, layer:'town'}),
           Object.freeze({src:'./asset/gate_small.png', left:30, bottom:4.8, height:39, layer:'town'}),
           Object.freeze({src:'./asset/house_poor.png', left:73, bottom:5.5, height:26, layer:'town', flip:true}),
-          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_man_01.png', left:19, bottom:5.8, height:13.5, layer:'people'}),
-          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_guard_01.png', left:64, bottom:5.8, height:14, layer:'people'})
+          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_man_01.png', left:19, bottom:4.8, height:23, layer:'people'}),
+          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_guard_01.png', left:64, bottom:4.8, height:24, layer:'people'})
         ])
       }),
       cityCommon: Object.freeze({
@@ -58,9 +58,9 @@
           Object.freeze({src:'./asset/house_normal.png', left:-5, bottom:5.5, height:31, layer:'town'}),
           Object.freeze({src:'./asset/stall.png', left:36, bottom:3.8, height:22, layer:'interactive'}),
           Object.freeze({src:'./asset/house_normal.png', left:64, bottom:5.5, height:30, layer:'town', flip:true}),
-          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_woman_01.png', left:18, bottom:5.8, height:13, layer:'people'}),
-          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_traveler_01.png', left:50, bottom:5.8, height:13.5, layer:'people'}),
-          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_child_01.png', left:79, bottom:5.8, height:9.5, layer:'people'})
+          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_woman_01.png', left:18, bottom:4.8, height:23, layer:'people'}),
+          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_traveler_01.png', left:50, bottom:4.8, height:23, layer:'people'}),
+          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_child_01.png', left:79, bottom:4.8, height:16, layer:'people'})
         ])
       }),
       upperArea: Object.freeze({
@@ -69,7 +69,7 @@
         objects: Object.freeze([
           Object.freeze({src:'./asset/house_upper.png', left:3, bottom:5.8, height:35, layer:'town'}),
           Object.freeze({src:'./asset/house_upper.png', left:51, bottom:5.8, height:34, layer:'town', flip:true}),
-          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_beggar_01.png', left:45, bottom:5.8, height:12.5, layer:'people'})
+          Object.freeze({src:'https://raw.githubusercontent.com/namiyukuta-cmd/iroiro/main/SHOP/asset/Npc/npc_beggar_01.png', left:45, bottom:4.8, height:22, layer:'people'})
         ])
       })
     }),
