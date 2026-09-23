8b849e93b1e3689ffe598b7193c2d9efc0eff670
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-23 15:02:22 +0900

    Sync SHOP build for far background 84 percent

--- SHOP/SHOP_data.js
+++ SHOP/SHOP_data.js
@@ -127,8 +127,8 @@
     }),
 
     links: Object.freeze({
-      newGame: './SHOP_top.html?new=1&build=20260923-1512-shift-town-up',
-      continueGame: './SHOP_top.html?continue=1&build=20260923-1512-shift-town-up',
+      newGame: './SHOP_top.html?new=1&build=20260923-farbg84-v2',
+      continueGame: './SHOP_top.html?continue=1&build=20260923-farbg84-v2',
       backToGames: '../index.html',
       backToShopMenu: './SHOP_index.html'
     })
