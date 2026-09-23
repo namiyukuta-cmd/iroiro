33c715be8e16201f6d8d56be83c53fc966ac4596
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-23 15:15:16 +0900

    Sync SHOP fresh page URLs

--- SHOP/SHOP_data.js
+++ SHOP/SHOP_data.js
@@ -127,8 +127,8 @@
     }),
 
     links: Object.freeze({
-      newGame: './SHOP_top.html?new=1&build=20260923-farbg-px-up-v1',
-      continueGame: './SHOP_top.html?continue=1&build=20260923-farbg-px-up-v1',
+      newGame: './SHOP_top.html?new=1&build=20260923-far-layer-shift-v3',
+      continueGame: './SHOP_top.html?continue=1&build=20260923-far-layer-shift-v3',
       backToGames: '../index.html',
       backToShopMenu: './SHOP_index.html'
     })
