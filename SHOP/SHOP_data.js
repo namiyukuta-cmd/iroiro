ec700614048d65a9fad22adc0be8569c8c480750
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-23 19:17:49 +0900

    Sync SHOP build for top text readability

--- SHOP/SHOP_data.js
+++ SHOP/SHOP_data.js
@@ -152,8 +152,8 @@
     }),
 
     links: Object.freeze({
-      newGame: './SHOP_top.html?new=1&build=20260923-compact-calendar-prayer-v1',
-      continueGame: './SHOP_top.html?continue=1&build=20260923-compact-calendar-prayer-v1',
+      newGame: './SHOP_top.html?new=1&build=20260923-top-text-readable-v1',
+      continueGame: './SHOP_top.html?continue=1&build=20260923-top-text-readable-v1',
       backToGames: '../index.html',
       backToShopMenu: './SHOP_index.html'
     })
