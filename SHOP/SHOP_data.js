4266c95529edad860237ffe7bf8d3e702354bd1c
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-23 15:07:31 +0900

    Sync SHOP build for pixel background shift

--- SHOP/SHOP_data.js
+++ SHOP/SHOP_data.js
@@ -127,8 +127,8 @@
     }),
 
     links: Object.freeze({
-      newGame: './SHOP_top.html?new=1&build=20260923-farbg84-v2',
-      continueGame: './SHOP_top.html?continue=1&build=20260923-farbg84-v2',
+      newGame: './SHOP_top.html?new=1&build=20260923-farbg-px-up-v1',
+      continueGame: './SHOP_top.html?continue=1&build=20260923-farbg-px-up-v1',
       backToGames: '../index.html',
       backToShopMenu: './SHOP_index.html'
     })
