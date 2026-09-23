774e0782c95b41cece546ef589f26d3af073425c
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-23 16:19:28 +0900

    Sync SHOP scene-navigation build

--- SHOP/SHOP_data.js
+++ SHOP/SHOP_data.js
@@ -127,8 +127,8 @@
     }),
 
     links: Object.freeze({
-      newGame: './SHOP_top.html?new=1&build=20260923-bottom-workspace-v1',
-      continueGame: './SHOP_top.html?continue=1&build=20260923-bottom-workspace-v1',
+      newGame: './SHOP_top.html?new=1&build=20260923-scene-arrows-v1',
+      continueGame: './SHOP_top.html?continue=1&build=20260923-scene-arrows-v1',
       backToGames: '../index.html',
       backToShopMenu: './SHOP_index.html'
     })
