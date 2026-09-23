3566bf43cb1b048e4fcd913cc3df211afe6e98b7
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-23 14:02:15 +0900

    Restore SHOP data before unintended action edits

--- SHOP/SHOP_data.js
+++ SHOP/SHOP_data.js
@@ -127,8 +127,8 @@
     }),
 
     links: Object.freeze({
-      newGame: './SHOP_top.html?new=1&build=20260923-1400-action-force',
-      continueGame: './SHOP_top.html?continue=1&build=20260923-1400-action-force',
+      newGame: './SHOP_top.html?new=1&build=20260923-1332-match-reference',
+      continueGame: './SHOP_top.html?continue=1&build=20260923-1332-match-reference',
       backToGames: '../index.html',
       backToShopMenu: './SHOP_index.html'
     })
