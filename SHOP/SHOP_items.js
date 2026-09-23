2079c77a9ecd737b5d71ce67c51c1d1558e4022f
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-23 19:49:26 +0900

    Set item sell values in copper currency

--- SHOP/SHOP_items.js
+++ SHOP/SHOP_items.js
@@ -2,10 +2,10 @@
   'use strict';
 
   const ITEMS = Object.freeze({
-    rag: Object.freeze({id:'rag', name:'布切れ', sellPrice:1}),
-    bottle: Object.freeze({id:'bottle', name:'空き瓶', sellPrice:2}),
-    wood: Object.freeze({id:'wood', name:'木片', sellPrice:1}),
-    scrap: Object.freeze({id:'scrap', name:'金属片', sellPrice:3})
+    rag: Object.freeze({id:'rag', name:'布切れ', sellMinCopper:2, sellMaxCopper:4}),
+    bottle: Object.freeze({id:'bottle', name:'空き瓶', sellMinCopper:3, sellMaxCopper:5}),
+    wood: Object.freeze({id:'wood', name:'木片', sellMinCopper:2, sellMaxCopper:5}),
+    scrap: Object.freeze({id:'scrap', name:'金属片', sellMinCopper:8, sellMaxCopper:10})
   });
 
   function getInventoryCount(state){
