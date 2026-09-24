ef9fe7995f48d126917b409d5e1ab600aefc94e0
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-24 10:39:45 +0900

    Sync SHOP links to life progression build

--- SHOP/SHOP_data.js
+++ SHOP/SHOP_data.js
@@ -338,8 +338,8 @@
     }),
 
     links: Object.freeze({
-      newGame: './SHOP_top.html?new=1&build=20260924-social-rules-v1',
-      continueGame: './SHOP_top.html?continue=1&build=20260924-social-rules-v1',
+      newGame: './SHOP_top.html?new=1&build=20260924-life-route-v1',
+      continueGame: './SHOP_top.html?continue=1&build=20260924-life-route-v1',
       backToGames: '../index.html',
       backToShopMenu: './SHOP_index.html'
     })
