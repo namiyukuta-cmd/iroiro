ce83b3e5b1178332e306098ce6cb61e34a6515cc
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-24 10:44:19 +0900

    Add unique HMW vocabulary batch 013

---
+++ HMW/js/dialogue/language/vocabulary-013-unique-common.js
@@ -0,0 +1,160 @@
+(() => {
+  "use strict";
+  const V = window.HMW?.Dialogue?.Vocabulary;
+  if (!V) throw new Error("Vocabulary registry must load first.");
+  V.register([
+  {
+    "lemma": "airline",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "shower",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "vacation",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "excited",
+    "pos": "adjective",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "calendar",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "candle",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "castle",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "diary",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "envelope",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "harbor",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "highway",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "passport",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "refrigerator",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "supermarket",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "taxi",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "theater",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  },
+  {
+    "lemma": "unlock",
+    "pos": "verb",
+    "tags": [
+      "common",
+      "conversation",
+      "unique013"
+    ]
+  }
+]);
+})();
