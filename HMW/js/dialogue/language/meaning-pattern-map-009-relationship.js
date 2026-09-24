91f2d59cb4cef480c17f4fdf0d66baea0da33928
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-24 12:02:38 +0900

    Use dynamic HMW relationship answer patterns

--- HMW/js/dialogue/language/meaning-pattern-map-009-relationship.js
+++ HMW/js/dialogue/language/meaning-pattern-map-009-relationship.js
@@ -3,10 +3,10 @@
   window.HMW=window.HMW||{}; HMW.Dialogue=HMW.Dialogue||{};
   HMW.Dialogue.MEANING_PATTERN_MAP=Object.assign(HMW.Dialogue.MEANING_PATTERN_MAP||{},{
     STATE_RELATIONSHIP_STATUS:[
-      {surfaceOverride:"We are {RELATIONSHIP}.",weight:5}
+      {pattern:"RELATIONSHIP_STATUS",slots:{RELATIONSHIP:"close"},weight:5}
     ],
     STATE_FEELINGS_TOWARD_HEROINE:[
-      {surfaceOverride:"I feel {FEELING} about you.",weight:5}
+      {pattern:"FEELINGS_TOWARD_HEROINE",slots:{FEELING:"affection"},weight:5}
     ]
   });
 })();
