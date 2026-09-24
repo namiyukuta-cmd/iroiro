a5d06f6bfff348fae0c3822f9caeb69e299b21c5
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-24 09:20:19 +0900

    Sync SHOP links to tappable place glow build

--- SHOP/SHOP_data.js
+++ SHOP/SHOP_data.js
@@ -234,8 +234,8 @@
     }),
 
     links: Object.freeze({
-      newGame: './SHOP_top.html?new=1&build=20260923-simple-stall-v1',
-      continueGame: './SHOP_top.html?continue=1&build=20260923-simple-stall-v1',
+      newGame: './SHOP_top.html?new=1&build=20260924-tappable-place-glow-v1',
+      continueGame: './SHOP_top.html?continue=1&build=20260924-tappable-place-glow-v1',
       backToGames: '../index.html',
       backToShopMenu: './SHOP_index.html'
     })
