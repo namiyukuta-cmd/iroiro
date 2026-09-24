85e20fab989490bb60c005236aa1a1e9c545b0a0
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-24 10:38:06 +0900

    Add unique HMW vocabulary batch 012

---
+++ HMW/js/dialogue/language/vocabulary-012-unique-common.js
@@ -0,0 +1,718 @@
+(() => {
+  "use strict";
+  const V = window.HMW?.Dialogue?.Vocabulary;
+  if (!V) throw new Error("Vocabulary registry must load first.");
+  V.register([
+  {
+    "lemma": "alcohol",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "angle",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "art",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "authority",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "award",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "beer",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "bell",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "bike",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "bit",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "cake",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "captain",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "celebration",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "chain",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "challenge",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "champion",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "charity",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "chart",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "chicken",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "cigarette",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "concert",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "contest",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "council",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "cousin",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "district",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "elevator",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "entertainment",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "exam",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "exchange",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "fan",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "football",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "fuel",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "generation",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "income",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "independence",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "insect",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "length",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "location",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "manner",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "measure",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "media",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "mood",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "mud",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "newspaper",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "ocean",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "operation",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "pilot",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "plane",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "plant",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "professor",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "property",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "race",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "reaction",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "region",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "relation",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "repair",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "row",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "security",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "sentence",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "series",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "site",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "software",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "soil",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "speaker",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "stomach",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "style",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "surface",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "tent",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "test",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "title",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "tool",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "topic",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "union",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "vehicle",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "version",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "video",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "website",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "wine",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "writer",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  },
+  {
+    "lemma": "zone",
+    "pos": "noun",
+    "tags": [
+      "common",
+      "conversation",
+      "unique012"
+    ]
+  }
+]);
+})();
